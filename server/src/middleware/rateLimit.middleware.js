0f06573374e845a246d23dc7a8e6f298f031e454
samarth8804 <samarthmittal0808@gmail.com>
2026-05-13 19:15:50 +0530

    Add rate limiting middleware to control request frequency

---
+++ server/src/middleware/rateLimit.middleware.js
@@ -0,0 +1,17 @@
+import rateLimit from "express-rate-limit";
+
+export const authRateLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+
+  max: 20,
+
+  standardHeaders: true,
+
+  legacyHeaders: false,
+
+  message: {
+    success: false,
+    message:
+      "Too many requests, please try again later",
+  },
+});
